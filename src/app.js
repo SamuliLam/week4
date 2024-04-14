3cf76d4ba5659f38dc873886ba02d78c75900959
Samuli Lamminmäki <samuli.lamminmaki@metropolia.fi>
2024-04-14 18:19:13 +0300

    d

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
-import api from './api.index.js';
+import api from './index.js';
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use('/api/v1', api);
+
+export default app;
